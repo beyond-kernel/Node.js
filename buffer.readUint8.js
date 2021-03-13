a17e85cd249364602dc374e20a88043be45f5442
Rafael Nogueira <rafanog.linux@hotmail.com>
2021-03-13 17:52:29 -0300

    padronização de buffers

---
+++ buffer.readUint8.js
@@ -0,0 +1,9 @@
+var buf = new Buffer.alloc(4);
+
+//escreve valores no buffer
+buf.writeUInt8(0x63, 0);
+buf.writeUInt8(0x61, 1);
+buf.writeUInt8(0x74, 2);
+buf.writeUInt8(0x73, 3);
+
+console.log(buf.toString());
--- bufferToJsonToString.js
+++ bufferToJsonToString.js
@@ -1,12 +1,12 @@
 "use strict";
 
-let buf = new Buffer('This is my example');
+let buf = new Buffer.from('This is my example');
 let json = JSON.stringify(buf);
 
 console.log(json);
 
 //buff to json
-let buf2 = new Buffer(JSON.parse(json).data);
+let buf2 = new Buffer.from(JSON.parse(json).data);
 
 //buff to string
 console.log(buf2.toString());
